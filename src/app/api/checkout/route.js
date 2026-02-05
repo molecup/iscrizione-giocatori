200c1c6225035f6016f145518ac7c912fd6d882a
marcomag416 <marcomag416@gmail.com>
2026-02-05 18:11:55 +0100

    feat: billing adress in stripe checkout

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -86,6 +86,7 @@ export async function POST(request) {
       allow_promotion_codes: true,
       locale: "it",
       customer_email: customerEmail,
+      billing_address_collection: "required",
       metadata,
     });
 
